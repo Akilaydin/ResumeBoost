1cf0ce4c98c186621c8fed0d70282b663c13cfa7
Artem Ovchinnikov <akilaydin2@gmail.com>
2025-08-11 13:30:46 +0300

    Updated scripts to use data-qa

--- ScriptsConstants.cs
+++ ScriptsConstants.cs
@@ -2,31 +2,14 @@
 {
     public class ScriptsConstants
     {
-        public const string AuthorizationCheckScript = @"
-            (function check_auth() {
-                let elements = document.querySelectorAll('div');
-                let filtered = Array.from(elements).filter(e => e.textContent.trim() === 'Мои резюме');
-                return filtered.length;
-            })();";
+        public const string AuthorizationCheckScript = @"(function check_auth() { return document.querySelectorAll('[data-qa=""mainmenu_profileAndResumes""]').length; })();";
 
-        public const string FindResumeScript = @"
-    (function check_resumes_col() {
-        let buttons = document.querySelectorAll('div[class^=""magritte-button-view""]');
-        let filtered = Array.from(buttons).filter(el => 
-            el.innerText.trim() === 'Поднять в поиске'
-        );
-        return filtered.length;
-    })();";
+        public const string FindResumeScript = @"(function check_resumes_col() { return document.querySelectorAll('[data-qa^=""resume-update-button""]').length; })();";
 
-        public const string UpdateResumeScript = @"
-    (function update_resumes() {
-        let buttons = document.querySelectorAll('div[class^=""magritte-button-view""]');
-        let filtered = Array.from(buttons).filter(el => 
-            el.innerText.trim() === 'Поднять в поиске'
-        );
-        filtered.forEach(el => el.click());
-        return filtered.length;
-    })();";
-
+        public const string UpdateResumeScript = @" (function update_resumes() {
+            const buttons = document.querySelectorAll('[data-qa^=""resume-update-button""]');
+            buttons.forEach(btn => btn.click());
+            return buttons.length;
+        })();";
     }
 }
